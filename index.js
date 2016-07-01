5eb59193d05c35a24be183d21ded02f393e06c1f
Matthew Dahl <mdahl@ciena.com>
2016-06-30 17:02:24 -0700

    Fix issues

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const Funnel = require('broccoli-funnel')
 const mergeTrees = require('broccoli-merge-trees')
 const path = require('path')
 
@@ -8,8 +9,19 @@ module.exports = {
 
   treeForAddon (tree) {
     const dagrePath = path.dirname(require.resolve('dagre/index.js'))
-    const dagreTree = this.treeGenerator(dagrePath)
-    const trees = mergeTrees([dagreTree, tree], {
+
+    const dagreFunnel = new Funnel(dagrePath, {
+      include: [
+        'lib/**/*.js',
+        'index.js'
+      ]
+    })
+
+    if (!tree) {
+      return this._super.treeForAddon.call(this, dagreFunnel)
+    }
+
+    const trees = mergeTrees([dagreFunnel, tree], {
       overwrite: true
     })
 
--- tests/unit/index-exports-test.js
+++ tests/unit/index-exports-test.js
@@ -1,7 +1,21 @@
 import {expect} from 'chai'
-import {it} from 'mocha'
-import dagre from 'dagre'
+import {describe, it} from 'mocha'
+import {debug, layout, util, version} from 'dagre'
 
-it('dagre is a single module that has been exported', function () {
-  expect(typeof dagre).to.equal('object')
+describe('dagre', () => {
+  it('exports debug', () => {
+    expect(debug).to.be.defined
+  })
+
+  it('exports layout', () => {
+    expect(layout).to.be.defined
+  })
+
+  it('exports util', () => {
+    expect(util).to.be.defined
+  })
+
+  it('exports version', () => {
+    expect(version).to.be.defined
+  })
 })
