bdc282fc54e25dacd35f2e15fe81c0b7cd54ebff
Matthew Dahl <mdahl@ciena.com>
2016-07-02 07:04:06 -0400

    Fix blueprint and upgrade ember-graphlib

--- blueprints/ember-dagre/index.js
+++ blueprints/ember-dagre/index.js
@@ -2,9 +2,10 @@ module.exports = {
   afterInstall: function () {
     return this.addAddonsToProject({
       packages: [
-        {name: 'ember-graphlib', target: '>=0.0.1 <2.0.0'},
+        {name: 'ember-graphlib', target: '>=0.0.5 <2.0.0'},
         {name: 'ember-lodash', target: '>=0.0.9 <2.0.0'}
-      })
+      ]
+    })
   },
 
   normalizeEntityName: function () {
