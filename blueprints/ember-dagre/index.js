2f3e1596bfc0bcaedc4a99555c0d3b96ba7f32f3
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-03 10:02:16 -0700

    Upgrade to lodash 4:

--- blueprints/ember-dagre/index.js
+++ blueprints/ember-dagre/index.js
@@ -2,8 +2,8 @@ module.exports = {
   afterInstall: function () {
     return this.addAddonsToProject({
       packages: [
-        {name: 'ember-graphlib', target: '>=0.0.5 <2.0.0'},
-        {name: 'ember-lodash', target: '>=0.0.9 <2.0.0'}
+        {name: 'ember-graphlib', target: '>=0.1.0 <2.0.0'},
+        {name: 'ember-lodash-shim', target: '0.1.2'}
       ]
     })
   },
