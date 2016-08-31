2dce0c561853787afdd4c4a60005e049e6c65de2
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-31 08:53:40 -0700

    Add tests

---
+++ tests/unit/chai.js
@@ -0,0 +1,13 @@
+import chai from 'chai'
+
+chai.config.includeStack = true
+
+/*
+ * Fix Chai"s `notProperty` which passes when an object has a property but its
+ * value is undefined.
+ */
+chai.assert.notProperty = function (obj, prop) {
+  chai.assert(!(prop in obj), 'Found prop ' + prop + ' in ' + obj + ' with value ' + obj[prop])
+}
+
+export default chai
---
+++ tests/unit/order/order-test.js
@@ -0,0 +1,48 @@
+import {expect} from 'chai'
+import order from 'dagre/order'
+import crossCount from 'dagre/order/cross-count'
+import {buildLayerMatrix} from 'dagre/util'
+import {Graph} from 'graphlib'
+import _ from 'lodash'
+import {beforeEach, describe, it} from 'mocha'
+
+describe('order', function () {
+  var g
+
+  beforeEach(function () {
+    g = new Graph()
+      .setDefaultEdgeLabel({ weight: 1 })
+  })
+
+  it('does not add crossings to a tree structure', function () {
+    g.setNode('a', { rank: 1 })
+    _.forEach(['b', 'e'], function (v) { g.setNode(v, { rank: 2 }) })
+    _.forEach(['c', 'd', 'f'], function (v) { g.setNode(v, { rank: 3 }) })
+    g.setPath(['a', 'b', 'c'])
+    g.setEdge('b', 'd')
+    g.setPath(['a', 'e', 'f'])
+    order(g)
+    var layering = buildLayerMatrix(g)
+    expect(crossCount(g, layering)).to.equal(0)
+  })
+
+  it('can solve a simple graph', function () {
+    // This graph resulted in a single crossing for previous versions of dagre.
+    _.forEach(['a', 'd'], function (v) { g.setNode(v, { rank: 1 }) })
+    _.forEach(['b', 'f', 'e'], function (v) { g.setNode(v, { rank: 2 }) })
+    _.forEach(['c', 'g'], function (v) { g.setNode(v, { rank: 3 }) })
+    order(g)
+    var layering = buildLayerMatrix(g)
+    expect(crossCount(g, layering)).to.equal(0)
+  })
+
+  it('can minimize crossings', function () {
+    g.setNode('a', { rank: 1 })
+    _.forEach(['b', 'e', 'g'], function (v) { g.setNode(v, { rank: 2 }) })
+    _.forEach(['c', 'f', 'h'], function (v) { g.setNode(v, { rank: 3 }) })
+    g.setNode('d', { rank: 4 })
+    order(g)
+    var layering = buildLayerMatrix(g)
+    expect(crossCount(g, layering)).to.be.lte(1)
+  })
+})
